81712b5aedf5b5ef90b8d487df44009c6c9f33a1
Neil Acharya <nacharya114@gmail.com>
2018-09-03 14:17:48 -0400

    Added database_upload script to backend folder

---
+++ smoothiesAPI/database_upload.py
@@ -0,0 +1,35 @@
+import os
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    passwd="yourpassword",
+    database="mydatabase"
+    )
+
+class Recipe():
+    def __init__(self, **kwargs):
+        self.title = kwargs.pop('title')
+        self.image = kwargs.pop('image')
+        self.rating = kwargs.pop('rating')
+        self.direction = kwargs.pop('direction')
+        self.serving = kwargs.pop('serving')
+        self.cal = kwargs.pop('cal')
+        self.fat = kwargs.pop('fat')
+        self.carb = kwargs.pop('carb') 
+        self.protein = kwargs.pop('protein')
+        self.ingredients = kwargs.pop('ingredients')
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    mycursor = mydb.cursor()
